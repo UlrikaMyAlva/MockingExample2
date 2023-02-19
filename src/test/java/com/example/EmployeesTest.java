9e673a74813dc45123a89b004f465bc923c27ca8
Ulrika Eriksson <ulrika.m.a.eriksson@gmail.com>
2023-02-19 20:43:18 +0100

    Added two tests, tesId and testWithMock

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -16,19 +16,29 @@ class EmployeesTest {
 
     }
 
+    //Tests with own parameters
     @Test
     void testId () {
-
+        Employee employee = new Employee("1212", 30000);
+        employee.setPaid(true);
+        assertTrue(employee.isPaid());
     }
 
-    //Test using Mockito
+    //Tests using Mockito
+    @Test
+    void testIdWithMock () {
+        Employee employee = mock(Employee.class);
+        when(employee.isPaid()).thenReturn(true);
+        assertTrue(employee.isPaid());
+    }
+
     @SuppressWarnings("unchecked")
     @Test
     public void spyOnEmployeeRepository() { //Testklass med Mockito
         EmployeeRepository dummyEmployee = mock(EmployeeRepository.class);
         List<Employee> spyList = spy(dummyEmployee.findAll());
-        when(spyList.size()).thenReturn(5);
-        assertTrue(spyList.size()==5);
+        when(spyList.size()).thenReturn(3);
+        assertTrue(spyList.size()==3);
     }
 
     void testDoubleEmployeeRepository() { //Skapa egna inputs att testa med
