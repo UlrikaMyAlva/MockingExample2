f5e6ee1bf9ced0485b839c459ddc14fd9a4fc6f0
Ulrika Eriksson <ulrika.m.a.eriksson@gmail.com>
2023-02-19 21:00:05 +0100

    Added test for Employee ID

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,9 +1,9 @@
 package com.example;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
@@ -15,15 +15,17 @@ class EmployeesTest {
 
     @Mock
     Employee employee = mock(Employee.class);
+    EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
 
     @BeforeEach
     void Init() {
-
+        MockitoAnnotations.initMocks(this);
     }
 
     //Tests with own parameters
     @Test
-    void testId () {
+    void testIfEmployeeIsPaidByCreatingAnEmployee () {
+        //Here an employee is created to test if you can pay them.
         Employee employee = new Employee("1212", 30000);
         employee.setPaid(true);
         assertTrue(employee.isPaid());
@@ -31,19 +33,31 @@ class EmployeesTest {
 
     //Tests using Mockito
     @Test
-    void testIdWithMock () {
-        //Employee employee = mock(Employee.class);
+    void testEmployeeId () {
+        //Here you do not need to create an employee because you use mock.
+        //The employee is already created under the annotation @mock
+        when(employee.getId()).thenReturn("1212");
+        assertEquals("1212",employee.getId());
+    }
+
+    @Test
+    void testEmployeeIsPaid () {
         when(employee.isPaid()).thenReturn(true);
         assertTrue(employee.isPaid());
     }
 
-    @SuppressWarnings("unchecked")
+    @Test
+    void testEmployeeGetSalary() {
+        when(employee.getSalary()).thenReturn(30000.00);
+        assertEquals(30000.00, employee.getSalary());
+    }
+
     @Test
     public void spyOnEmployeeRepository() { //Testklass med Mockito
-        EmployeeRepository dummyEmployee = mock(EmployeeRepository.class);
-        List<Employee> spyList = spy(dummyEmployee.findAll());
+       //Spy is used to spy on the list in employeeRepository
+        List<Employee> spyList = spy(employeeRepository.findAll());
         when(spyList.size()).thenReturn(3);
-        assertTrue(spyList.size()==3);
+        assertEquals(3, spyList.size());
     }
 
     void testDoubleEmployeeRepository() { //Skapa egna inputs att testa med
